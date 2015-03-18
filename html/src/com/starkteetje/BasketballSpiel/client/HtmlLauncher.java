efb5aca6fb6e764b1a6ad1c74437fb73635cc4ab
unknown <starkteetje@BAMBERG.imp.fu-berlin.de>
2015-03-18 15:51:10 +0100

    initial commit

---
+++ html/src/com/starkteetje/BasketballSpiel/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.starkteetje.BasketballSpiel.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.starkteetje.BasketballSpiel.BasketballSpiel;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new BasketballSpiel();
+        }
+}
